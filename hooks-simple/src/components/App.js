3c1d426eda5a46141c3c27883477b2190f3c37bf
juan carlos gonzales montes <pope624@hotmail.com>
2020-03-09 10:14:26 -0500

    Create App.js

---
+++ hooks-simple/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () =>{
+
+    return (
+        <div className="ui container grid">
+            <div className="ui row">
+                hola a todos desde react
+            </div>
+        </div>
+
+    );
+}
+
+export default App;
